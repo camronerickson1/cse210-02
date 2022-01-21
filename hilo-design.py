8e8023c1283ce06f5c3e60188955c67281a8eafd
camronerickson1 <eri20010@byui.edu>
2022-01-20 20:46:15 -0700

    Initial Design

---
+++ hilo-design.py
@@ -0,0 +1,45 @@
+"""Classes needed and possible methods: (new file for each class)
+
+Camron: Director attributes, start_game, get_inputs, README file.
+Monika: do_outputs, change_score.
+Jonathan: keep_playing, Card(), main file.
+
+Director
+
+    Attributes:
+        is_playing=boolean
+        score=int
+        current_card
+        new_card
+
+
+    Methods:
+        start game:
+            call:
+                get_inputs
+                do_outputs
+
+        get_inputs:
+            display current_card
+            player_guess
+            
+            
+        do_outputs:
+            new_card = Card()
+            display new_card value
+            call "change_score()"
+            call "keep_playing()"
+            
+
+        change_score:
+            update the score
+            display score
+
+        keep_playing:
+            ask question
+            change "is_playing" if needed
+            set new card to current card
+
+Card
+    create card
+"""
